d2d51208521ce123652605636759863228a9c18e
Jhansi-123762 <kuchikajhansirani@gmail.com>
2026-07-30 18:17:06 +0530

    CRUD API

--- database.py
+++ database.py
@@ -1,13 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"ssl": {}}
-)
-
+DATABASE_URL = "mysql+pymysql://root:Root%40123@localhost:3306/ecommerce"
 
 engine = create_engine(DATABASE_URL)
 
