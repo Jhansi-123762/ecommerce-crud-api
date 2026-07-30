5af6327941407fecaf7d805955fc883ba8f644b1
Jhansi-123762 <kuchikajhansirani@gmail.com>
2026-07-30 12:15:44 +0530

    use environment variable

--- database.py
+++ database.py
@@ -1,7 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "mysql+pymysql://root:Root%40123@localhost/shopping_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"ssl": {}}
+)
+
 
 engine = create_engine(DATABASE_URL)
 
