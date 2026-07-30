06d91e008078420d0a52d8df2b65778c470852eb
Jhansi-123762 <kuchikajhansirani@gmail.com>
2026-07-30 19:20:28 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # DATABASE_URL = "mysql+pymysql://root:Root%40123@localhost:3306/ecommerce"
-DATABASE_URL="mysql://avnadmin:AVNS_7pyjmqWYY1yrN5A9Qaw@creative-kuchikajhansirani-6090.j.aivencloud.com:27979/defaultdb?ssl-mode=REQUIRED"
+DATABASE_URL="mysql+pymysql://avnadmin:AVNS_7pyjmqWYY1yrN5A9Qaw@creative-kuchikajhansirani-6090.j.aivencloud.com:27979/defaultdb?ssl-mode=REQUIRED"
 
 engine = create_engine(DATABASE_URL)
 
